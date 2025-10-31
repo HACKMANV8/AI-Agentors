9fce5ed8dbcfeb9c609910c8baff192c2e47e904
rithikh321 <rithikh11@gmail.com>
2025-10-31 17:40:27 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,6 @@ import pandas as pd
 import numpy as np
 import uvicorn
 import os
-
 # ---------- Load model, scaler, and feature columns ----------
 def safe_load_pickle(filename):
     if not os.path.exists(filename):
