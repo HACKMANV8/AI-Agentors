ac953185c38135d55f63c7f2de8572fc22a3489c
rithikh321 <rithikh11@gmail.com>
2025-11-01 07:30:53 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ import uvicorn
 import os
 
 # ---------- Load model, scaler, and feature columns ----------
+
 def safe_load_pickle(filename):
     if not os.path.exists(filename):
         raise FileNotFoundError(f"❌ Missing required file: {filename}")
